c09abdf3e3315add436b4a84fa384d57f95e9426
Sourav Datta <soura.jagat@gmail.com>
2019-06-23 23:23:30 +0530

    Add files via upload

---
+++ scala.el
@@ -0,0 +1,25 @@
+(defun scala-project (project-name)
+  (interactive "sProject name: ")
+  (make-directory project-name)
+  (make-directory (format "%s/src/main/scala" project-name) t)
+  (make-directory (format "%s/src/main/test" project-name) t)
+  (make-directory (format "%s/project" project-name) t)
+  (write-region "" "" (format "%s/build.sbt" project-name))
+  (message "Done"))
+
+(defun replace-in-string (what with in)
+  (replace-regexp-in-string (regexp-quote what) with in nil 'literal))
+
+(defun scala-src-package (package-name)
+  (interactive "sPackage name: ")
+  (make-directory (format "%s/src/main/scala/%s"
+			  default-directory
+			  (replace-in-string "." "/" package-name))
+		  t))
+
+(defun scala-test-package (package-name)
+  (interactive "sPackage name: ")
+  (make-directory (format "%s/src/test/scala/%s"
+			  default-directory
+			  (replace-in-string "." "/" package-name))
+		  t))
